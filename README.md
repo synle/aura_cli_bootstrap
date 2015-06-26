c5b1b3c2dc03b062b768f40b90ec5840e6571e45
Sy Le <sle@sle-ltm.internal.salesforce.com>
2015-06-26 15:06:21 -0700

    fix issue with absolute path to auracli modules

--- README.md
+++ README.md
@@ -7,18 +7,28 @@ Installation
 
 	#then run npm link
 	npm link
+
+	#then use it as
+	#wizard
+	auracli
+
+	#create app
+	auracli app 
+
+	#create cmp
+	auracli cmp
 ```
 
 Usage
 ```
-./auracli
+	./auracli
 
-prompt: File type: cmp or app?:  (cmp) 
-prompt: File Name?:  (TestComponent) 
-prompt: Does this have a Test? (Y or N):  (Y) 
-prompt: Does this have a Controller? (Y or N):  (Y) 
-prompt: Does this have a Renderer? (Y or N):  (Y) 
-prompt: Does this have a Helper? (Y or N):  (Y) 
-prompt: Does this have a Css? (Y or N):  (Y) 
-prompt: Your Host? Used to open the component on browser:  (http://localhost:9090/uitest/) 
+	prompt: File type: cmp or app?:  (cmp) 
+	prompt: File Name?:  (TestComponent) 
+	prompt: Does this have a Test? (Y or N):  (Y) 
+	prompt: Does this have a Controller? (Y or N):  (Y) 
+	prompt: Does this have a Renderer? (Y or N):  (Y) 
+	prompt: Does this have a Helper? (Y or N):  (Y) 
+	prompt: Does this have a Css? (Y or N):  (Y) 
+	prompt: Your Host? Used to open the component on browser:  (http://localhost:9090/uitest/) 
 ```
--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 #! /usr/bin/env node
-
 // dependencies
 // external
 var colors = require('colors');
 var prompt = require('prompt');
 var fs = require('fs');
+var path = require('path');
+
 // internal
 //schema
 var schema = require('./prompt_schema.js');
 var templatePath = {
-        app: './templates/app.template',
-        cmp: './templates/cmp.template',
-        controller: './templates/controller.template',
-        css: './templates/css.template',
-        helper: './templates/helper.template',
-        renderer: './templates/renderer.template'
-    }
-    //read the template files
-var appTemplate = fs.readFileSync(templatePath.app, 'utf8');
-var cmpTemplate = fs.readFileSync(templatePath.cmp, 'utf8');
-var controllerTemplate = fs.readFileSync(templatePath.controller, 'utf8');
-var cssTemplate = fs.readFileSync(templatePath.css, 'utf8');
-var helperTemplate = fs.readFileSync(templatePath.helper, 'utf8');
-var rendererTemplate = fs.readFileSync(templatePath.renderer, 'utf8');
+    app: 'templates/app.template',
+    cmp: 'templates/cmp.template',
+    controller: 'templates/controller.template',
+    css: 'templates/css.template',
+    helper: 'templates/helper.template',
+    renderer: 'templates/renderer.template'
+};
+
+
+var auracliModulePath = path.join( path.dirname(fs.realpathSync(__filename)), '/');
+
+console.log(auracliModulePath + templatePath.app);
+//read the template files
+var appTemplate = fs.readFileSync(auracliModulePath + templatePath.app, 'utf8');
+var cmpTemplate = fs.readFileSync(auracliModulePath + templatePath.cmp, 'utf8');
+var controllerTemplate = fs.readFileSync(auracliModulePath + templatePath.controller, 'utf8');
+var cssTemplate = fs.readFileSync(auracliModulePath + templatePath.css, 'utf8');
+var helperTemplate = fs.readFileSync(auracliModulePath + templatePath.helper, 'utf8');
+var rendererTemplate = fs.readFileSync(auracliModulePath + templatePath.renderer, 'utf8');
 
 
 //variable
@@ -38,7 +44,6 @@ var componentUrl;
 
 //check command line arguments
 process.argv.forEach(function(val, index, array) {
-    console.log(val.toUpperCase().replace('--', '') === 'CMP')
     switch(val.toUpperCase().replace('--', '')){
         case 'CMP':
             filetype = 'cmp';
@@ -51,7 +56,7 @@ process.argv.forEach(function(val, index, array) {
             break;
     }
 });
-console.log(Object.keys(schema.properties));
+
 
 //start prompt here
 prompt.start();
@@ -65,6 +70,21 @@ prompt.get(schema, function(err, result) {
     hasHelper = (result.hasHelper || '').toUpperCase().indexOf('Y') >= 0;
     hasCss = (result.hasCss || '').toUpperCase().indexOf('Y') >= 0;
     
-    var componentUrl = 'http://localhost:9090/uitest/tabset_overflowTest.cmp';
+    var componentUrl = 'http://localhost:9090/uitest/tabset_overflowTest.cmp';  
+
+
+    var isValidDir = true;
+    var curDirFiles = fs.readdirSync(__dirname);
+    curDirFiles.forEach(function(val){
+        if (val.indexOf('Controller.js') >= 0 ||
+        val.indexOf('Helper.js') >= 0 ||
+        val.indexOf('Renderer.js') >= 0 ||
+        val.indexOf('Test.js') >= 0 ||
+        val.indexOf('.cmp')  >= 0|| val.indexOf('.cmp')  >= 0){
+            console.log('Found invalid files: '.red) + ' : ' + __dirname + val;
+        }
+    });
+
+    console.log(__dirname);
     console.log(filename, filetype, hasTest, hasController, hasRenderer, hasHelper, hasCss);
 });
