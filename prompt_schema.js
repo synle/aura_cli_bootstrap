f341b6dbf41b24c67e0bc679ddaab679f507828e
Sy Le <sle@sle-ltm.internal.salesforce.com>
2015-06-26 14:30:23 -0700

    added colors

--- prompt_schema.js
+++ prompt_schema.js
@@ -1,42 +1,42 @@
-module.exports =  {
+module.exports = {
     'properties': {
         'type': {
-            'message': 'File type: a component or an app?',
+            'message': 'File type: ' + 'cmp'.bold.blue + ' or ' + 'app'.bold.blue + '?',
             'required': true,
             'default': 'cmp'
         },
         'filename': {
-        	'message': 'filename?',
-        	'required': true,
-        	'default': 'TestComponent'
+            'message': 'File Name'.blue.bold + '?',
+            'required': true,
+            'default': 'TestComponent'
         },
         'hasTest': {
-        	'message': 'Does this have a Test? (Y or N)',
-        	'required': true,
-        	'default': 'Y'
+            'message': 'Does this have a ' + 'Test'.bold.blue + '? (' + 'Y'.bold.green + ' or ' + 'N'.bold.red + ')',
+            'required': true,
+            'default': 'Y'
         },
         'hasController': {
-        	'message': 'Does this have a Controller? (Y or N)',
-        	'required': true,
-        	'default': 'Y'
+            'message': 'Does this have a ' + 'Controller'.bold.blue + '? (' + 'Y'.bold.green + ' or ' + 'N'.bold.red + ')',
+            'required': true,
+            'default': 'Y'
         },
         'hasRenderer': {
-        	'message': 'Does this have a Renderer? (Y or N)',
-        	'required': true,
-        	'default': 'Y'
+            'message': 'Does this have a ' + 'Renderer'.bold.blue + '? (' + 'Y'.bold.green + ' or ' + 'N'.bold.red + ')',
+            'required': true,
+            'default': 'Y'
         },
         'hasHelper': {
-        	'message': 'Does this have a Helper? (Y or N)',
-        	'required': true,
-        	'default': 'Y'
+            'message': 'Does this have a ' + 'Helper'.bold.blue + '? (' + 'Y'.bold.green + ' or ' + 'N'.bold.red + ')',
+            'required': true,
+            'default': 'Y'
         },
         'hasCss': {
-        	'message': 'Does this have a Css? (Y or N)',
-        	'required': true,
-        	'default': 'Y'
+            'message': 'Does this have a ' + 'Css'.bold.blue + '? (' + 'Y'.bold.green + ' or ' + 'N'.bold.red + ')',
+            'required': true,
+            'default': 'Y'
         },
         'appHost': {
-            'message': 'Your Host? Used to open the component on browser',
+            'message': 'Your Host'.bold.blue + '? Used to open the component on browser',
             'required': true,
             'default': 'http://localhost:9090/uitest/'
         }
