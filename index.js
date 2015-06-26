371808fd4b9db13cbbeac4dd4ba597c3f56a1e23
Sy Le <sle@sle-ltm.internal.salesforce.com>
2015-06-26 15:17:48 -0700

    handle invalid dir

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ var templatePath = {
 };
 
 
+//aura cli absolute path
 var auracliModulePath = path.join( path.dirname(fs.realpathSync(__filename)), '/');
+var cwd = process.cwd();
 
-console.log(auracliModulePath + templatePath.app);
 //read the template files
 var appTemplate = fs.readFileSync(auracliModulePath + templatePath.app, 'utf8');
 var cmpTemplate = fs.readFileSync(auracliModulePath + templatePath.cmp, 'utf8');
@@ -58,33 +59,51 @@ process.argv.forEach(function(val, index, array) {
 });
 
 
+//pre-error
+var isValidDir = true;
+var curDirFiles = fs.readdirSync(cwd);
+var invalidFilesFound = [];
+curDirFiles.forEach(function(val){
+    if (val.indexOf('Controller.js') >= 0 ||
+    val.indexOf('Helper.js') >= 0 ||
+    val.indexOf('Renderer.js') >= 0 ||
+    val.indexOf('Test.js') >= 0 ||
+    val.indexOf('.cmp')  >= 0|| val.indexOf('.cmp')  >= 0){
+        // console.log('Error: Found invalid file: '.bold.red + path.join(cwd + '/' + val).green);
+        invalidFilesFound.push(val);
+        isValidDir = false;
+    }
+});
+
+if (isValidDir === false){
+    console.log('Error'.red.bold + ': current directory '+cwd.bold+' already contained some definitions of Aura. Please change to a new directory without any Aura files and try this command again.');
+    console.log('Invalid files:'.bold.blue);
+    console.log(invalidFilesFound.join('\n'));
+    return;
+}
+
+
 //start prompt here
 prompt.start();
 prompt.get(schema, function(err, result) {
     filetype = (result.filetype || '').toUpperCase().indexOf('CMP') >= 0 ? 'cmp' : 'app';
-    appHost = result.appHost;
     filename = result.filename;
+    appHost = result.appHost;
     hasTest = (result.hasTest || '').toUpperCase().indexOf('Y') >= 0;
     hasController = (result.hasController || '').toUpperCase().indexOf('Y') >= 0;
     hasRenderer = (result.hasRenderer || '').toUpperCase().indexOf('Y') >= 0;
     hasHelper = (result.hasHelper || '').toUpperCase().indexOf('Y') >= 0;
     hasCss = (result.hasCss || '').toUpperCase().indexOf('Y') >= 0;
     
-    var componentUrl = 'http://localhost:9090/uitest/tabset_overflowTest.cmp';  
+    var componentUrl = 'http://localhost:9090/uitest/tabset_overflowTest.cmp';
 
 
-    var isValidDir = true;
-    var curDirFiles = fs.readdirSync(__dirname);
-    curDirFiles.forEach(function(val){
-        if (val.indexOf('Controller.js') >= 0 ||
-        val.indexOf('Helper.js') >= 0 ||
-        val.indexOf('Renderer.js') >= 0 ||
-        val.indexOf('Test.js') >= 0 ||
-        val.indexOf('.cmp')  >= 0|| val.indexOf('.cmp')  >= 0){
-            console.log('Found invalid files: '.red) + ' : ' + __dirname + val;
-        }
-    });
+    if (fs.existsSync(cwd + '/' + filename)) {
+        //already existed
+        console.log('Error: '.bold.red + ' component or application of the same name already ' + filename.green + ' exist in the directory ' + cwd.green);
+        return;
+    }
+    
+    console.log('Creating the component');
 
-    console.log(__dirname);
-    console.log(filename, filetype, hasTest, hasController, hasRenderer, hasHelper, hasCss);
 });
